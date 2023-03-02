bdac86e9fc1168df0e103abeb106b75cc19534cf
Nguyễn Huỳnh Anh Khoa <522h0046@student.tdtu.edu.vn>
2023-03-02 22:47:50 +0700

    fix bai14

--- week_1/522H0046_NguyenHuynhAnhKhoa/NguyenHuynhAnhKhoa_Bai14.java
+++ week_1/522H0046_NguyenHuynhAnhKhoa/NguyenHuynhAnhKhoa_Bai14.java
@@ -24,40 +24,39 @@ public class NguyenHuynhAnhKhoa_Bai14 {
 
     public static void xuLyThanhToan(int itemPrice) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("The price of this item is " + itemPrice + "$, please enter the amount of money:");
-            int money = sc.nextInt();
-            if (money < itemPrice) {
-                System.out.println("Not enough money to buy this item. Please enter again");
-            } else {
-                int change = money - itemPrice;
-                System.out.println("Your change is " + change);
-                break;
-            }
+        System.out.println("The price of this item is " + itemPrice + "$, please enter the amount of money:");
+        int money = sc.nextInt();
+        if (money < itemPrice) {
+            System.out.println("Not enough money to buy this item. Please enter again");
+        } else {
+            int change = money - itemPrice;
+            System.out.println("Your change is " + change + "$");
         }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        inThongBao();
-        int itemChoice = nhapLuaChon(sc);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            inThongBao();
+            int itemChoice = nhapLuaChon(sc);
 
-        switch (itemChoice) {
-            case 1:
-                xuLyThanhToan(2);
-                break;
-            case 2:
-                xuLyThanhToan(1);
-                break;
-            case 3:
-                xuLyThanhToan(3);
-                break;
-            case 4:
-                xuLyThanhToan(4);
-                break;
-            default:
-                System.out.println("Machine is shutting down...");
-                break;
+            switch (itemChoice) {
+                case 1:
+                    xuLyThanhToan(2);
+                    break;
+                case 2:
+                    xuLyThanhToan(1);
+                    break;
+                case 3:
+                    xuLyThanhToan(3);
+                    break;
+                case 4:
+                    xuLyThanhToan(4);
+                    break;
+                default:
+                    System.out.println("Machine is shutting down...");
+                    return;
+            }
         }
     }
 }
