661b33dc6277ebcba2acda0c2543ae3f91005bed
Nguyễn Huỳnh Anh Khoa <522h0046@student.tdtu.edu.vn>
2023-03-02 21:36:36 +0700

    import week 1

--- week_1/522H0046_NguyenHuynhAnhKhoa/NguyenHuynhAnhKhoa_Bai04.java
+++ week_1/522H0046_NguyenHuynhAnhKhoa/NguyenHuynhAnhKhoa_Bai04.java
@@ -7,8 +7,8 @@ public class NguyenHuynhAnhKhoa_Bai04 {
 		
 		System.out.print("Nhap vao nhiet do C: ");
 		double c_temp = sc.nextDouble();
-		
-		double f_temp = (9/5) * c_temp + 32;
+
+		double f_temp = (9.0 / 5.0) * c_temp + 32.0;
 
 		System.out.println("Nhiet do F: " + f_temp);
 	}
